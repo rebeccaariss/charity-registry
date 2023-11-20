af4cf5c69f6e0473edfabb9869bd7100a098d073
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-11-20 16:58:34 -0500

    Feat: org profile page uses queries to build up a orgs profile page grabbing the basic org information and all current and past projects with items tied to that orgs project

--- express-back-end/db/queries/items.js
+++ express-back-end/db/queries/items.js
@@ -1,9 +1,21 @@
 const db = require("../connection");
 
+const getItemsByProjectId = (projectId) => {
+  return db.query(
+    `SELECT
+      id,
+      item_description,
+      quantity_needed,
+      urgency_level,
+      status,
+      item_price
+    FROM items
+    WHERE project_id = $1;`,
+    [projectId]
+  ).then((data) => {
+    return data.rows;
+  });
+};
 
 
-
-
-
-
-module.exports = {  };
+module.exports = { getItemsByProjectId };
