ba9a521f3f2abf0f249127b6fe5ddc986140d2ea
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-23 13:40:49 -0500

    Set up cookie-session and database queries for checking credentials

--- express-back-end/db/queries/users.js
+++ express-back-end/db/queries/users.js
@@ -3,26 +3,30 @@ const db = require("../connection");
   // check if user exists/check credentials against the database
   const checkUser = async (credentials) => {
     try {
+      const { username, password } = credentials;
       const data = await db
         .query(
-          ``,
-          []
+          `SELECT * FROM users WHERE username = $1 AND password = $2`,
+          [username, password]
         );
+        return data.rows[0];
     } catch (err) {
-      console.log("", err);
+      console.log("Error checking user credentials", err);
     }
   }
 
   // check if organization exists/check credentials against the database
   const checkOrganization = async (credentials) => {
     try {
+      const { username, password } = credentials;
       const data = await db
         .query(
-          ``,
-          []
+          `SELECT * FROM organizations WHERE username = $1 AND password = $2`,
+          [username, password]
         );
+        return data.rows[0];
     } catch (err) {
-      console.log("", err);
+      console.log("Error checking organization credentials", err);
     }
   }
 
