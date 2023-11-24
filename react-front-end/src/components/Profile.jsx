3581edf3ce2f5ff921d5dad8444eda23fcda1db2
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-24 18:41:08 -0500

    Uninstall axios

--- react-front-end/src/components/Profile.jsx
+++ react-front-end/src/components/Profile.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Card, Container, Row, Col, Image, Button } from 'react-bootstrap';
 import '../styles/Profile.css';
 
