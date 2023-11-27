2e55e866dda8dda67f16a007d0ffe83e20f0e461
andrijana85 <topalovic.andrijana@yahoo.com>
2023-11-27 15:26:06 -0500

    Implement DonationList, DonationListItem Component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import "./App.css";
 
-import { Route, Routes } from "react-router-dom"
-import  ProjectList  from "./components/ProjectList"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { OrganizationList } from './components/OrganizationList';
-import Profile from "./components/Profile"
+import { Route, Routes } from "react-router-dom";
+import ProjectList from "./components/ProjectList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { OrganizationList } from "./components/OrganizationList";
+import Profile from "./components/Profile";
+import DonationList from "./components/DonationList";
 
 function App() {
   return (
     <Routes>
-     <Route path='api/projects/followed-projects' element={<ProjectList />} />
-     <Route path="/" element={<OrganizationList />} />
-     <Route path="/profile/:id" element={<Profile />} />
+      <Route path="api/projects/followed-projects" element={<ProjectList />} />
+      <Route path="/api/donations/user/:id" element={<DonationList />} />
+      <Route path="/" element={<OrganizationList />} />
+      <Route path="/profile/:id" element={<Profile />} />
     </Routes>
   );
 }
