f84349dc24daa5224c7de0f5222161dcb9b119e8
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-11-27 15:26:11 -0500

    Create org page

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -5,6 +5,7 @@ import  ProjectList  from "./components/ProjectList"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { OrganizationList } from './components/OrganizationList';
 import Profile from "./components/Profile"
+import { CreateOrg } from './components/CreateOrg';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Route path='api/projects/followed-projects' element={<ProjectList />} />
      <Route path="/" element={<OrganizationList />} />
      <Route path="/profile/:id" element={<Profile />} />
+     <Route path="/orgcreate" element={<CreateOrg />} />
     </Routes>
   );
 }
