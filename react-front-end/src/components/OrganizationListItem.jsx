ad9ac2d4baed90a6acc94d9f40f0e4baabe7251e
andrijana85 <topalovic.andrijana@yahoo.com>
2023-11-28 16:14:39 -0500

    Implement NavBar

--- react-front-end/src/components/OrganizationListItem.jsx
+++ react-front-end/src/components/OrganizationListItem.jsx
@@ -1,24 +1,64 @@
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
 export function OrganizationListItem({ org }) {
   const profilePath = `/profile/${org.id}`;
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{org.name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{org.category}</Card.Subtitle>
 
-        <Card.Text>
-          <strong>{org.urgent_requests} urgent requests</strong><br />
-          <strong>{org.active_requests} active requests</strong>
-        </Card.Text>
-        
-        <Card.Text>{org.website}</Card.Text>
-        <Card.Text>{org.address}</Card.Text>
-        <Card.Text>{org.bio}</Card.Text>
+  const activeReuestsStyle = {
+    color: "green",
+    fontWeight: "bold",
+  };
+
+  const urgentRequestsStyle = {
+    color: "red",
+    fontWeight: "bold",
+  };
+
+  return (
+    <Card className="h-100 shadow-sm position-relative w-85 mx-auto">
+      <Card.Body className="d-flex flex-column">
+        <div>
+          <Card.Title>{org.name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {org.category}
+          </Card.Subtitle>
+
+          <Card.Text>
+            <FontAwesomeIcon
+              icon={faExclamationCircle}
+              className="fa-light fa-circle-exclamation fa-fade fa-xs mr-1 text-danger"
+            />
+            <strong style={urgentRequestsStyle}>
+              {" "}
+              {org.urgent_requests} urgent requests
+            </strong>
+            <br />
+            <FontAwesomeIcon
+              icon={faHandHoldingHeart}
+              style={{ marginRight: "10px", color: "green" }}
+            />
+            <strong style={activeReuestsStyle}>
+              {org.active_requests} active requests
+            </strong>
+          </Card.Text>
+
+          <Card.Text>
+            <span className="text-muted">Website: {org.website}</span>
+            <br />
+            <span className="text-muted">Address: {org.address}</span>
+          </Card.Text>
+          <Card.Text>{org.bio}</Card.Text>
+        </div>
         {/* TODO: hook up profile path */}
-        <Link to={profilePath} className="btn btn-link">Read More</Link>
+        <Link
+          to={profilePath}
+          className="btn btn-link position-absolute bottom-0 end-0 m-1"
+        >
+          Read More
+        </Link>
       </Card.Body>
     </Card>
   );
--- react-front-end/src/components/ProjectList.jsx
+++ react-front-end/src/components/ProjectList.jsx
@@ -1,19 +1,21 @@
 import ProjectListItem from "./ProjectListItem";
+import { Nav } from "react-bootstrap";
+import NavBar from "./NavBar";
 
 const ProjectList = (props) => {
   const { projects } = props;
 
   return (
-    <div
-      className="project-list-container"
-      style={{ margin: "20px" }}
-    >
-      <ul className="list-unstyled">
-        {projects.map((project) => (
-          <ProjectListItem key={project["id"]} project={project} />
-        ))}
-      </ul>
-    </div>
+    <>
+      <NavBar />
+      <div className="project-list-container" style={{ margin: "20px" }}>
+        <ul className="list-unstyled">
+          {projects.map((project) => (
+            <ProjectListItem key={project["id"]} project={project} />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
