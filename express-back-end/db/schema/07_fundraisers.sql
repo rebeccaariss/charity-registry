8c66e233321b85693678488feb12bb214dd50f1e
andrijana85 <topalovic.andrijana@yahoo.com>
2023-11-29 13:41:51 -0500

    Implement POST, PUT and DELETE fundraisers

--- express-back-end/db/schema/07_fundraisers.sql
+++ express-back-end/db/schema/07_fundraisers.sql
@@ -5,7 +5,7 @@ CREATE TABLE "fundraisers"(
     "project_id" INTEGER NOT NULL,
     "amount_raised" DECIMAL(10, 2) NOT NULL,
     "goal_amount" DECIMAL(10, 2) NOT NULL,
-    "status" VARCHAR(255) NOT NULL,
+    "status" VARCHAR(255) DEFAULT 'Active',
     "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
