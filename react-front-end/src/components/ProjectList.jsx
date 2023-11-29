7a9ac3d139557340bb89f2bf8b41f5a454e0dbf4
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-28 21:39:39 -0500

    Move NavBar out of ProjectList and into Profile component

--- react-front-end/src/components/ProjectList.jsx
+++ react-front-end/src/components/ProjectList.jsx
@@ -1,21 +1,16 @@
 import ProjectListItem from "./ProjectListItem";
-import { Nav } from "react-bootstrap";
-import NavBar from "./NavBar";
 
 const ProjectList = (props) => {
   const { projects } = props;
 
   return (
-    <>
-      <NavBar />
-      <div className="project-list-container" style={{ margin: "20px" }}>
-        <ul className="list-unstyled">
-          {projects.map((project) => (
-            <ProjectListItem key={project["id"]} project={project} />
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="project-list-container" style={{ margin: "20px" }}>
+      <ul className="list-unstyled">
+        {projects.map((project) => (
+          <ProjectListItem key={project["id"]} project={project} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
