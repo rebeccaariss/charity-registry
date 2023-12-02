40caed0f155b6f3ae44c0486e4fcc6e81a76c498
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-12-02 11:14:59 -0500

    Feat: Delete Button

--- react-front-end/src/components/Item.jsx
+++ react-front-end/src/components/Item.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Item = ({ item, onDonate, donationAmount, updateDonationAmount, toggleDonationInput, selectedItemId }) => {
+const Item = ({ item, onDonate, donationAmount, updateDonationAmount, toggleDonationInput, selectedItemId, onDelete }) => {
 
   if (!item) {
     return null; // Render nothing if item is undefined
@@ -11,6 +13,9 @@ const Item = ({ item, onDonate, donationAmount, updateDonationAmount, toggleDona
       <span onClick={() => toggleDonationInput(item.id)} style={{cursor: "pointer"}}>
         {item.item_description} - {item.quantity_donated}/{item.quantity_needed} donated
       </span>
+      <span onClick={() => onDelete(item.id)} style={{ cursor: "pointer" }}>
+  <FontAwesomeIcon icon={faTimes} />
+    </span>
       {selectedItemId === item.id && (
         <div>
           <input 
